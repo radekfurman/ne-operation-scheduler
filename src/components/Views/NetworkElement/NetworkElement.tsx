a8799d69710b7ab8551406ad8d2da09d18928a47
Radoslaw Furman <radoslaw.furman@nokia.com>
2021-01-28 18:35:44 +0100

    Added network elements table with test data

--- src/components/Views/NetworkElement/NetworkElement.tsx
+++ src/components/Views/NetworkElement/NetworkElement.tsx
@@ -1,5 +1,44 @@
+import {
+    makeStyles,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from '@material-ui/core';
 import React from 'react';
+import { networksElementTestData } from './networkElementsData';
+
+const useStyles = makeStyles({
+    root: {}
+
+  });
 
 export const NetworkElement: React.FunctionComponent<{}> = () => {
-    return <div>Network element view</div>;
+    const classes = useStyles();
+    return (
+        <TableContainer>
+            <Table className={classes.root} aria-label='simple table' stickyHeader={true}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell align='left'>IP Address</TableCell>
+                        <TableCell align='left'>Type</TableCell>
+                        <TableCell align='left'>DN</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {networksElementTestData.map((networkElement) => (
+                        <TableRow key={networkElement.id}>
+                            <TableCell component='th' scope='row'>
+                                {networkElement.ipAddress}
+                            </TableCell>
+                            <TableCell align='left'>{networkElement.type}</TableCell>
+                            <TableCell align='left'>{networkElement.distName}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 };
---
+++ src/components/Views/NetworkElement/networkElementsData.ts
@@ -0,0 +1,207 @@
+export interface NetworkElement {
+    id: number;
+    ipAddress: string;
+    type: string;
+    distName: string;
+}
+
+export const networksElementTestData: NetworkElement[] = [
+    {
+        id: 1,
+        ipAddress: '10.10.10.10',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-01',
+    },
+    {
+        id: 2,
+        ipAddress: '10.10.10.12',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-02',
+    },
+    {
+        id: 3,
+        ipAddress: '10.10.10.13',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-03',
+    },
+    {
+        id: 4,
+        ipAddress: '10.10.10.14',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-04',
+    },
+    {
+        id: 5,
+        ipAddress: '10.10.10.15',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-05',
+    },
+    {
+        id: 6,
+        ipAddress: '10.10.10.16',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-06',
+    },
+    {
+        id: 7,
+        ipAddress: '10.10.10.17',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-07',
+    },
+    {
+        id: 8,
+        ipAddress: '10.10.10.18',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-08',
+    },
+    {
+        id: 9,
+        ipAddress: '10.10.10.19',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-09',
+    },
+    {
+        id: 10,
+        ipAddress: '10.10.10.10',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-10',
+    },
+    {
+        id: 11,
+        ipAddress: '10.10.10.11',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-11',
+    },
+    {
+        id: 12,
+        ipAddress: '10.10.10.12',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-12',
+    },
+    {
+        id: 13,
+        ipAddress: '10.10.10.13',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-13',
+    },
+    {
+        id: 14,
+        ipAddress: '10.10.10.14',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-14',
+    },
+    {
+        id: 15,
+        ipAddress: '10.10.10.15',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-15',
+    },
+    {
+        id: 16,
+        ipAddress: '10.10.10.16',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-16',
+    },
+    {
+        id: 17,
+        ipAddress: '10.10.10.17',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-17',
+    },
+    {
+        id: 18,
+        ipAddress: '10.10.10.18',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-18',
+    },
+    {
+        id: 19,
+        ipAddress: '10.10.10.19',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-19',
+    },
+    {
+        id: 20,
+        ipAddress: '10.10.10.20',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-20',
+    },
+    {
+        id: 21,
+        ipAddress: '10.10.10.21',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-21',
+    },
+    {
+        id: 22,
+        ipAddress: '10.10.10.22',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-22',
+    },
+    {
+        id: 23,
+        ipAddress: '10.10.10.23',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-23',
+    },
+    {
+        id: 24,
+        ipAddress: '10.10.10.24',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-24',
+    },
+    {
+        id: 25,
+        ipAddress: '10.10.10.25',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-25',
+    },
+    {
+        id: 26,
+        ipAddress: '10.10.10.26',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-26',
+    },
+    {
+        id: 27,
+        ipAddress: '10.10.10.27',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-27',
+    },
+    {
+        id: 28,
+        ipAddress: '10.10.10.28',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-28',
+    },
+    {
+        id: 29,
+        ipAddress: '10.10.10.29',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-29',
+    },
+    {
+        id: 30,
+        ipAddress: '10.10.10.30',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-30',
+    },
+    {
+        id: 31,
+        ipAddress: '10.10.10.34',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-34',
+    },
+    {
+        id: 32,
+        ipAddress: '10.10.10.35',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-35',
+    },
+    {
+        id: 33,
+        ipAddress: '10.10.10.36',
+        type: 'eNodeB',
+        distName: 'PLMN-PLMN/MRBTS-4321/LNBTS-36',
+    },
+];
